38c5f3043e447a4fc375308f6550ac4d5c2b8c9e
jasmiinee <jasmiine.lje@gmail.com>
2024-06-17 00:12:29 +0800

    Firebase Storage

--- app/src/main/java/com/example/biznus/ListActivity.java
+++ app/src/main/java/com/example/biznus/ListActivity.java
@@ -1,18 +1,155 @@
 package com.example.biznus;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.ActivityNavigatorDestinationBuilderKt;
+
+import com.canhub.cropper.CropImage;
+import com.canhub.cropper.CropImageContract;
+import com.canhub.cropper.CropImageContractOptions;
+import com.canhub.cropper.CropImageOptions;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class ListActivity extends AppCompatActivity {
+
+    Uri imageUri;
+    String myUrl = "";
+    StorageTask uploadTask;
+    StorageReference storageReference;
+
+    ImageView close, image;
+    TextView list;
+    EditText editTextTitle, editTextPrice, editTextDescription;
+    ImageButton imageButton;
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        close = findViewById(R.id.close);
+        image = findViewById(R.id.image);
+        list = findViewById(R.id.list);
+        editTextTitle = findViewById(R.id.title);
+        editTextPrice = findViewById(R.id.price);
+        editTextDescription = findViewById(R.id.description);
+        imageButton = findViewById(R.id.addImage);
+
+        // TODO: all fields should not be empty, condition spinner, upload to database
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ListActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        list.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (imageUri == null) {
+                    Toast.makeText(ListActivity.this, "Image Required", Toast.LENGTH_SHORT).show();
+                } else {
+                    uploadImage();
+                }
+
+            }
+        });
+
+
+    }
+
+    private void uploadImage() {
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Uploading File...");
+        progressDialog.show();
+
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss", Locale.ENGLISH);
+        Date now = new Date();
+        String filename = formatter.format(now);
+        storageReference = FirebaseStorage.getInstance().getReference("images/"+filename);
+
+        storageReference.putFile(imageUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        image.setImageURI(null);
+                        Toast.makeText(ListActivity.this, "Successfully Uploaded", Toast.LENGTH_SHORT).show();
+
+                        if (progressDialog.isShowing()) {
+                            progressDialog.dismiss();
+                        }
+                        Intent intent = new Intent(ListActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        if (progressDialog.isShowing()) {
+                            progressDialog.dismiss();
+                        }
+                        Toast.makeText(ListActivity.this, "Failed to Upload", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 100);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 100 && data != null && data.getData() != null) {
+            imageUri = data.getData();
+            image.setImageURI(imageUri);
+        }
     }
 }
